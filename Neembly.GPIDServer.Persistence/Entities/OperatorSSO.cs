2538aa640c10b19f3c6ed8ac252d6b520f90ada4
gerwinlorenzo <gerwinlorenzo@neembly.com>
2023-02-03 18:24:40 +0800

    sso table

---
+++ Neembly.GPIDServer.Persistence/Entities/OperatorSSO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Neembly.GPIDServer.Persistence.Entities
+{
+    public class OperatorSSO
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public int OperatorId { get; set; }
+        [StringLength(50)]
+        public string AuthProvider { get; set; }
+        [StringLength(4096)]
+        public string Parameters { get; set; }
+        public bool IsEnabled { get; set; } 
+    }
+}
