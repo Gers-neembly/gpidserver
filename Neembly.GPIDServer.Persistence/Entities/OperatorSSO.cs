5eb2ae72eeb1c4cb9d43a569773f02ee62580a74
gerwinlorenzo <gerwinlorenzo@neembly.com>
2023-02-07 18:56:25 +0800

    added autoregister flags

--- Neembly.GPIDServer.Persistence/Entities/OperatorSSO.cs
+++ Neembly.GPIDServer.Persistence/Entities/OperatorSSO.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -14,6 +15,9 @@ namespace Neembly.GPIDServer.Persistence.Entities
         public string AuthProvider { get; set; }
         [StringLength(4096)]
         public string Parameters { get; set; }
-        public bool IsEnabled { get; set; } 
+        [DefaultValue(true)]
+        public bool IsEnabled { get; set; }
+        [DefaultValue(true)]
+        public bool AutoRegister { get; set; } = true;
     }
 }
