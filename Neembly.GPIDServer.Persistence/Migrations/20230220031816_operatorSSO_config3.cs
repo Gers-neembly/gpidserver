f32a620c212161412c593e25a535e6fa3de5848c
gerwinlorenzo <gerwinlorenzo@neembly.com>
2023-02-20 11:29:21 +0800

    added description remove operator

---
+++ Neembly.GPIDServer.Persistence/Migrations/20230220031816_operatorSSO_config3.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Neembly.GPIDServer.Persistence.Migrations
+{
+    public partial class operatorSSO_config3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OperatorId",
+                table: "OperatorSSO");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OperatorId",
+                table: "OperatorSSO",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
