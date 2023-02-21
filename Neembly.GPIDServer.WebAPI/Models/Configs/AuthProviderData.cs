257ca8541a855dc5f3371c8fd79d5cfbd22deeea
gerwinlorenzo <gerwinlorenzo@neembly.com>
2023-02-21 17:44:02 +0800

    added Facebook SSO

--- Neembly.GPIDServer.WebAPI/Models/Configs/AuthProviderData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Neembly.GPIDServer.WebAPI.Models.Configs
-{
-    public class AuthProviderData
-    {
-        public string Client_Id { get; set; }
-        public string Client_Secret { get; set; }
-    }
-}
