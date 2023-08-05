05e2eee5d3c2f00d32475a9832900dbf7b91a534
Gerwin Lorenzo <gerwinlorenzo@neembly.com>
2023-08-05 11:19:40 +0000

    Revert "change oidc settings (pull request #192)"

--- Neembly.GPIDServer.WebAPI/DependencyInjectionConfigs.cs
+++ Neembly.GPIDServer.WebAPI/DependencyInjectionConfigs.cs
@@ -1,11 +1,9 @@
 ﻿using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Neembly.GPIDServer.Constants;
 using Neembly.GPIDServer.Persistence;
 using Neembly.GPIDServer.Persistence.Entities;
 using Neembly.GPIDServer.WebAPI.Filters;
@@ -14,7 +12,6 @@ using Neembly.GPIDServer.WebAPI.Models.Configs;
 using Neembly.GPIDServer.WebAPI.Queries;
 using Neembly.GPIDServer.WebAPI.Services;
 using System;
-using System.Linq;
 
 namespace Neembly.GPIDServer.WebAPI
 {
@@ -25,11 +22,10 @@ namespace Neembly.GPIDServer.WebAPI
         /// </summary>
         /// <param name="services"></param>
         /// <returns></returns>
-        /// 
-
         public static IServiceCollection Add(this IServiceCollection services, IConfiguration configuration)
         {
             var paramList = Environment.GetCommandLineArgs();
+
             int offset = Array.FindIndex(paramList, m => m == "--webname");
             string webname = string.Empty;
             if (offset >= 0) webname = paramList[offset + 1];
@@ -63,18 +59,6 @@ namespace Neembly.GPIDServer.WebAPI
             configuration.Bind("AuthClientConfiguration", authClientConfig);
             services.AddSingleton(authClientConfig);
 
-            //authentication client config
-            var authAuthorityHosts = new AuthorityHost();
-            configuration.Bind("AuthorityHost", authAuthorityHosts);
-            services.AddSingleton(authAuthorityHosts);
-
-            var hostAuthority = string.Empty;
-
-            if (string.IsNullOrEmpty(webname))
-                hostAuthority = authAuthorityHosts.AuthorityHostList.Where(p => p.webname == "default").Select(p => p.webaddress).FirstOrDefault();
-            else
-                hostAuthority = authAuthorityHosts.AuthorityHostList.Where(p => p.webname == webname).Select(p => p.webaddress).FirstOrDefault();
-
             //add identity server
             services.AddIdentityServer()
                     .AddDeveloperSigningCredential()
@@ -83,8 +67,6 @@ namespace Neembly.GPIDServer.WebAPI
                     .AddInMemoryApiResources(Config.GetApiResources(authClientConfig.AuthClientResourcesList))
                     .AddInMemoryClients(Config.GetClients(authClientConfig.AuthClientInfoList))
                     .AddAspNetIdentity<AppUser>();
-
-            var signInOIDC = authClientConfig.AuthClientInfoList.Where(p => p.Type == GlobalConstants.AuthTypeGrantCode).FirstOrDefault();
 
             //identity option configs
             services.Configure<IdentityOptions>(o => {
@@ -98,27 +80,9 @@ namespace Neembly.GPIDServer.WebAPI
             services.AddTransient<IOperatorSSOQueries, OperatorSSOQueries>();
 
             // add authentications
-            services.AddAuthentication(options =>
-            {
-                options.DefaultScheme = "Cookies";
-                options.DefaultChallengeScheme = "oidc";
-            })
-            .AddGoogleAuth(services, webname)
-            .AddFacebookAuth(services, webname)
-            .AddCookie("Cookies")
-            .AddOpenIdConnect("oidc", options =>
-            {
-                options.Authority = hostAuthority;
-                options.RequireHttpsMetadata = false;       
-                options.ClientId = signInOIDC.ClientId;
-                options.ClientSecret = signInOIDC.SecretKey;
-                options.ResponseType = "code";      
-                options.SaveTokens = true;
-                options.Scope.Add("openid");
-                options.Scope.Add("email");
-                options.Scope.Add("profile");
-                options.Scope.Add("offline_access");
-            });
+            services.AddAuthentication()
+                    .AddGoogleAuth(services, webname)
+                    .AddFacebookAuth(services, webname);
 
             //mvc services
             services.AddMvc(options => 
