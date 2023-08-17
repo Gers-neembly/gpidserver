b6fadec01ec610205e46885740c0dbabe75e509a
gerwinlorenzo <gerwinlorenzo@neembly.com>
2023-08-17 19:54:09 +0800

    added new ssocontroller for telegram

---
+++ Neembly.GPIDServer.WebAPI/Models/oAuth/oAuthResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Neembly.GPIDServer.WebAPI.Models.oAuth
+{
+    public class oAuthResult
+    {
+        public bool result { get; set; }
+        public int playerId { get; set; }
+        public string displayUserName { get; set; }
+        public string tokenKey { get; set; }
+        public string authProvider { get; set; }
+    }
+}
